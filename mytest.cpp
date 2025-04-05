16aebdbd89963e72fb46eaabc5f9bd413f024931
ihshah512 <optiimran@gmail.com>
2025-04-05 12:07:09 -0400

    added functionlities, test benchs need review

---
+++ mytest.cpp
@@ -0,0 +1,496 @@
+// CMSC 341 - Spring 2025 - Project 3
+#include "squeue.h"
+#include <math.h>
+#include <algorithm>
+#include <random>
+#include <vector>
+using namespace std;
+
+enum RANDOM
+{
+    UNIFORMINT,
+    UNIFORMREAL,
+    NORMAL,
+    SHUFFLE
+};
+class Random
+{
+public:
+    Random() {}
+    Random(int min, int max, RANDOM type = UNIFORMINT, int mean = 50, int stdev = 20) : m_min(min), m_max(max), m_type(type)
+    {
+        if (type == NORMAL)
+        {
+            // the case of NORMAL to generate integer numbers with normal distribution
+            m_generator = std::mt19937(m_device());
+            // the data set will have the mean of 50 (default) and standard deviation of 20 (default)
+            // the mean and standard deviation can change by passing new values to constructor
+            m_normdist = std::normal_distribution<>(mean, stdev);
+        }
+        else if (type == UNIFORMINT)
+        {
+            // the case of UNIFORMINT to generate integer numbers
+            //  Using a fixed seed value generates always the same sequence
+            //  of pseudorandom numbers, e.g. reproducing scientific experiments
+            //  here it helps us with testing since the same sequence repeats
+            m_generator = std::mt19937(10); // 10 is the fixed seed value
+            m_unidist = std::uniform_int_distribution<>(min, max);
+        }
+        else if (type == UNIFORMREAL)
+        {                                   // the case of UNIFORMREAL to generate real numbers
+            m_generator = std::mt19937(10); // 10 is the fixed seed value
+            m_uniReal = std::uniform_real_distribution<double>((double)min, (double)max);
+        }
+        else
+        { // the case of SHUFFLE to generate every number only once
+            m_generator = std::mt19937(m_device());
+        }
+    }
+    void setSeed(int seedNum)
+    {
+        // we have set a default value for seed in constructor
+        // we can change the seed by calling this function after constructor call
+        // this gives us more randomness
+        m_generator = std::mt19937(seedNum);
+    }
+    void init(int min, int max)
+    {
+        m_min = min;
+        m_max = max;
+        m_type = UNIFORMINT;
+        m_generator = std::mt19937(10); // 10 is the fixed seed value
+        m_unidist = std::uniform_int_distribution<>(min, max);
+    }
+    void getShuffle(vector<int> &array)
+    {
+        // this function provides a list of all values between min and max
+        // in a random order, this function guarantees the uniqueness
+        // of every value in the list
+        // the user program creates the vector param and passes here
+        // here we populate the vector using m_min and m_max
+        for (int i = m_min; i <= m_max; i++)
+        {
+            array.push_back(i);
+        }
+        shuffle(array.begin(), array.end(), m_generator);
+    }
+
+    void getShuffle(int array[])
+    {
+        // this function provides a list of all values between min and max
+        // in a random order, this function guarantees the uniqueness
+        // of every value in the list
+        // the param array must be of the size (m_max-m_min+1)
+        // the user program creates the array and pass it here
+        vector<int> temp;
+        for (int i = m_min; i <= m_max; i++)
+        {
+            temp.push_back(i);
+        }
+        std::shuffle(temp.begin(), temp.end(), m_generator);
+        vector<int>::iterator it;
+        int i = 0;
+        for (it = temp.begin(); it != temp.end(); it++)
+        {
+            array[i] = *it;
+            i++;
+        }
+    }
+
+    int getRandNum()
+    {
+        // this function returns integer numbers
+        // the object must have been initialized to generate integers
+        int result = 0;
+        if (m_type == NORMAL)
+        {
+            // returns a random number in a set with normal distribution
+            // we limit random numbers by the min and max values
+            result = m_min - 1;
+            while (result < m_min || result > m_max)
+                result = m_normdist(m_generator);
+        }
+        else if (m_type == UNIFORMINT)
+        {
+            // this will generate a random number between min and max values
+            result = m_unidist(m_generator);
+        }
+        return result;
+    }
+
+    double getRealRandNum()
+    {
+        // this function returns real numbers
+        // the object must have been initialized to generate real numbers
+        double result = m_uniReal(m_generator);
+        // a trick to return numbers only with two deciaml points
+        // for example if result is 15.0378, function returns 15.03
+        // to round up we can use ceil function instead of floor
+        result = std::floor(result * 100.0) / 100.0;
+        return result;
+    }
+
+    string getRandString(int size)
+    {
+        // the parameter size specifies the length of string we ask for
+        // to use ASCII char the number range in constructor must be set to 97 - 122
+        // and the Random type must be UNIFORMINT (it is default in constructor)
+        string output = "";
+        for (int i = 0; i < size; i++)
+        {
+            output = output + (char)getRandNum();
+        }
+        return output;
+    }
+
+    int getMin() { return m_min; }
+    int getMax() { return m_max; }
+
+private:
+    int m_min;
+    int m_max;
+    RANDOM m_type;
+    std::random_device m_device;
+    std::mt19937 m_generator;
+    std::normal_distribution<> m_normdist;            // normal distribution
+    std::uniform_int_distribution<> m_unidist;        // integer uniform distribution
+    std::uniform_real_distribution<double> m_uniReal; // real uniform distribution
+};
+
+int priorityFn1(const Post &post); // works with a MAXHEAP
+int priorityFn2(const Post &post); // works with a MINHEAP
+
+class Tester
+{
+private:
+    // Helper to check heap property
+    bool checkHeapProperty(Post *node, prifn_t priFn, HEAPTYPE type)
+    {
+        if (!node)
+            return true;
+
+        int parentPri = priFn(*node);
+        if (node->m_left)
+        {
+            int leftPri = priFn(*node->m_left);
+            if ((type == MAXHEAP && parentPri < leftPri) ||
+                (type == MINHEAP && parentPri > leftPri))
+                return false;
+        }
+        if (node->m_right)
+        {
+            int rightPri = priFn(*node->m_right);
+            if ((type == MAXHEAP && parentPri < rightPri) ||
+                (type == MINHEAP && parentPri > rightPri))
+                return false;
+        }
+        return checkHeapProperty(node->m_left, priFn, type) &&
+               checkHeapProperty(node->m_right, priFn, type);
+    }
+
+    // Helper to check NPL values
+    bool checkNPL(Post *node)
+    {
+        if (!node)
+            return true;
+        int leftNPL = node->m_left ? node->m_left->m_npl : -1;
+        int rightNPL = node->m_right ? node->m_right->m_npl : -1;
+        return (node->m_npl == std::min(leftNPL, rightNPL) + 1) &&
+               (leftNPL >= rightNPL) &&
+               checkNPL(node->m_left) &&
+               checkNPL(node->m_right);
+    }
+
+public:
+    bool testMinHeapInsertion()
+    {
+        SQueue q(priorityFn2, MINHEAP, SKEW);
+        Random r(MINPOSTID, MAXPOSTID);
+        for (int i = 0; i < 300; i++)
+        {
+            q.insertPost(Post(r.getRandNum(), 100, 3, 25, 5));
+        }
+        return checkHeapProperty(q.m_heap, priorityFn2, MINHEAP);
+    }
+
+    bool testMaxHeapInsertion()
+    {
+        SQueue q(priorityFn1, MAXHEAP, SKEW);
+        Random r(MINPOSTID, MAXPOSTID);
+        for (int i = 0; i < 300; i++)
+        {
+            q.insertPost(Post(r.getRandNum(), 100, 3, 25, 5));
+        }
+        return checkHeapProperty(q.m_heap, priorityFn1, MAXHEAP);
+    }
+
+    bool testMinHeapRemoval()
+    {
+        SQueue q(priorityFn2, MINHEAP, SKEW);
+        Random r(MINPOSTID, MAXPOSTID);
+        std::vector<int> priorities;
+        for (int i = 0; i < 300; i++)
+        {
+            Post p(r.getRandNum(), 100, 3, i + 1, 5);
+            q.insertPost(p);
+            priorities.push_back(priorityFn2(p));
+        }
+        std::sort(priorities.begin(), priorities.end());
+        for (int i = 0; i < 300; i++)
+        {
+            try
+            {
+                if (priorityFn2(q.getNextPost()) != priorities[i])
+                    return false;
+            }
+            catch (...)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool testMaxHeapRemoval()
+    {
+        SQueue q(priorityFn1, MAXHEAP, SKEW);
+        Random r(MINPOSTID, MAXPOSTID);
+        std::vector<int> priorities;
+        for (int i = 0; i < 300; i++)
+        {
+            Post p(r.getRandNum(), i, 3, 25, 5);
+            q.insertPost(p);
+            priorities.push_back(priorityFn1(p));
+        }
+        std::sort(priorities.begin(), priorities.end(), std::greater<int>());
+        for (int i = 0; i < 300; i++)
+        {
+            try
+            {
+                if (priorityFn1(q.getNextPost()) != priorities[i])
+                    return false;
+            }
+            catch (...)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool testLeftistNPL()
+    {
+        SQueue q(priorityFn2, MINHEAP, LEFTIST);
+        Random r(MINPOSTID, MAXPOSTID);
+        for (int i = 0; i < 100; i++)
+        {
+            q.insertPost(Post(r.getRandNum(), 100, 3, 25, 5));
+        }
+        return checkNPL(q.m_heap);
+    }
+
+    bool testLeftistProperty()
+    {
+        SQueue q(priorityFn2, MINHEAP, LEFTIST);
+        Random r(MINPOSTID, MAXPOSTID);
+        for (int i = 0; i < 100; i++)
+        {
+            q.insertPost(Post(r.getRandNum(), 100, 3, 25, 5));
+        }
+        return checkHeapProperty(q.m_heap, priorityFn2, MINHEAP) && checkNPL(q.m_heap);
+    }
+
+    bool testPriorityChange()
+    {
+        SQueue q(priorityFn2, MINHEAP, SKEW);
+        Random r(MINPOSTID, MAXPOSTID);
+        for (int i = 0; i < 100; i++)
+        {
+            q.insertPost(Post(r.getRandNum(), 100, 3, 25, 5));
+        }
+        q.setPriorityFn(priorityFn1, MAXHEAP);
+        return checkHeapProperty(q.m_heap, priorityFn1, MAXHEAP);
+    }
+
+    bool testEmptyMerge()
+    {
+        SQueue q1(priorityFn2, MINHEAP, SKEW);
+        SQueue q2(priorityFn2, MINHEAP, SKEW);
+        Random r(MINPOSTID, MAXPOSTID);
+        for (int i = 0; i < 100; i++)
+        {
+            q2.insertPost(Post(r.getRandNum(), 100, 3, 25, 5));
+        }
+        try
+        {
+            q1.mergeWithQueue(q2);
+            return q2.numPosts() == 0 && checkHeapProperty(q1.m_heap, priorityFn2, MINHEAP);
+        }
+        catch (...)
+        {
+            return false;
+        }
+    }
+
+    bool testCopyConstructorNormal()
+    {
+        SQueue q1(priorityFn2, MINHEAP, SKEW);
+        Random r(MINPOSTID, MAXPOSTID);
+        for (int i = 0; i < 100; i++)
+        {
+            q1.insertPost(Post(r.getRandNum(), 100, 3, 25, 5));
+        }
+        SQueue q2(q1);
+        return q1.numPosts() == q2.numPosts() && checkHeapProperty(q2.m_heap, priorityFn2, MINHEAP);
+    }
+
+    bool testCopyConstructorEmpty()
+    {
+        SQueue q1(priorityFn2, MINHEAP, SKEW);
+        SQueue q2(q1);
+        return q2.numPosts() == 0;
+    }
+
+    bool testAssignmentNormal()
+    {
+        SQueue q1(priorityFn2, MINHEAP, SKEW);
+        SQueue q2(priorityFn2, MINHEAP, SKEW);
+        Random r(MINPOSTID, MAXPOSTID);
+        for (int i = 0; i < 100; i++)
+        {
+            q1.insertPost(Post(r.getRandNum(), 100, 3, 25, 5));
+        }
+        q2 = q1;
+        return q1.numPosts() == q2.numPosts() && checkHeapProperty(q2.m_heap, priorityFn2, MINHEAP);
+    }
+
+    bool testAssignmentEmpty()
+    {
+        SQueue q1(priorityFn2, MINHEAP, SKEW);
+        SQueue q2(priorityFn2, MINHEAP, SKEW);
+        q2 = q1;
+        return q2.numPosts() == 0;
+    }
+
+    bool testEmptyDequeue()
+    {
+        SQueue q(priorityFn2, MINHEAP, SKEW);
+        try
+        {
+            q.getNextPost();
+            return false;
+        }
+        catch (const std::out_of_range &)
+        {
+            return true;
+        }
+        catch (...)
+        {
+            return false;
+        }
+    }
+
+    bool testDifferentPriorityMerge()
+    {
+        SQueue q1(priorityFn1, MAXHEAP, SKEW);
+        SQueue q2(priorityFn2, MINHEAP, SKEW);
+        try
+        {
+            q1.mergeWithQueue(q2);
+            return false;
+        }
+        catch (const std::domain_error &)
+        {
+            return true;
+        }
+        catch (...)
+        {
+            return false;
+        }
+    }
+};
+
+int main()
+{
+    Random idGen(MINPOSTID, MAXPOSTID);
+    Random likesGen(MINLIKES, MAXLIKES);
+    Random timeGen(MINTIME, MAXTIME);
+    Random conLevelGen(MINCONLEVEL, MAXCONLEVEL);
+    Random interestGen(MININTERESTLEVEL, MAXINTERESTLEVEL);
+    SQueue queue1(priorityFn2, MINHEAP, LEFTIST);
+    for (int i = 0; i < 10; i++)
+    {
+        Post aPost(idGen.getRandNum(),
+                   likesGen.getRandNum(),
+                   conLevelGen.getRandNum(),
+                   timeGen.getRandNum(),
+                   interestGen.getRandNum());
+        queue1.insertPost(aPost);
+    }
+    cout << "\nDump of the leftist heap queue with priorityFn2 (MINHEAP):\n";
+    queue1.dump();
+    queue1.setStructure(SKEW);
+    cout << "\nDump of the skew heap queue with priorityFn2 (MINHEAP):\n";
+    queue1.dump();
+    cout << "\nPreorder traversal of the nodes in the queue with priorityFn2 (MINHEAP):\n";
+    queue1.printPostsQueue();
+    queue1.setPriorityFn(priorityFn1, MAXHEAP);
+    cout << "\nDump of the skew queue with priorityFn1 (MAXHEAP):\n";
+    queue1.dump();
+    cout << endl;
+    cout << "****Calling TEST functions as follow ****" << endl;
+
+    // creating obj of tester class for calling functions
+    Tester t;
+    // Calling tst functions here
+    cout << "MinHeap Insertion: " << (t.testMinHeapInsertion() ? "PASS" : "FAIL") << endl;
+    cout << "MaxHeap Insertion: " << (t.testMaxHeapInsertion() ? "PASS" : "FAIL") << endl;
+    cout << "MinHeap Removal: " << (t.testMinHeapRemoval() ? "PASS" : "FAIL") << endl;
+    cout << "MaxHeap Removal: " << (t.testMaxHeapRemoval() ? "PASS" : "FAIL") << endl;
+    cout << "Leftist NPL: " << (t.testLeftistNPL() ? "PASS" : "FAIL") << endl;
+    cout << "Leftist Property: " << (t.testLeftistProperty() ? "PASS" : "FAIL") << endl;
+    cout << "Priority Change: " << (t.testPriorityChange() ? "PASS" : "FAIL") << endl;
+    cout << "Empty Merge: " << (t.testEmptyMerge() ? "PASS" : "FAIL") << endl;
+    cout << "Copy Constructor Normal: " << (t.testCopyConstructorNormal() ? "PASS" : "FAIL") << endl;
+    cout << "Copy Constructor Empty: " << (t.testCopyConstructorEmpty() ? "PASS" : "FAIL") << endl;
+    cout << "Assignment Normal: " << (t.testAssignmentNormal() ? "PASS" : "FAIL") << endl;
+    cout << "Assignment Empty: " << (t.testAssignmentEmpty() ? "PASS" : "FAIL") << endl;
+    cout << "Empty Dequeue: " << (t.testEmptyDequeue() ? "PASS" : "FAIL") << endl;
+    cout << "Different Priority Merge: " << (t.testDifferentPriorityMerge() ? "PASS" : "FAIL") << endl;
+    return 0;
+}
+
+/* Priority functions */
+int priorityFn1(const Post &post)
+{
+    // needs MAXHEAP
+    // priority value is determined based on some criteria
+    // priority value falls in the range [1-510]
+    // the highest priority would be 500+10 = 510
+    // the lowest priority would be 0+1 = 1
+    // the larger value means the higher priority
+    int priority = post.getNumLikes() + post.getInterestLevel();
+    int minValue = 1;
+    int maxValue = 510;
+    if (priority >= minValue && priority <= maxValue)
+        return priority;
+    else
+        return 0; // this is an invalid order object
+}
+
+int priorityFn2(const Post &post)
+{
+    // needs MINHEAP
+    // priority value is determined based on some criteria
+    // priority value falls in the range [2-55]
+    // the highest priority would be 1+1 = 2
+    // the lowest priority would be 5+50 = 55
+    // the smaller value means the higher priority
+    int minValue = 2;
+    int maxValue = 55;
+    int priority = post.getPostTime() + post.getConnectLevel();
+    if (priority >= minValue && priority <= maxValue)
+        return priority;
+    else
+        return 0; // this is an invalid order object
+}
--- squeue.cpp
+++ squeue.cpp
@@ -1,78 +1,280 @@
 // CMSC 341 - Spring 2025 - Project 3
 #include "squeue.h"
-SQueue::SQueue(prifn_t priFn, HEAPTYPE heapType, STRUCTURE structure) {
-  
+
+// Constructor of Squeue
+SQueue::SQueue(prifn_t priFn, HEAPTYPE heapType, STRUCTURE structure)
+{
+
+  m_heap = nullptr;
+  m_size = 0;
+  m_priorFunc = priFn;
+  m_heapType = heapType;
+  m_structure = structure;
 }
-SQueue::~SQueue() {
-  
+// DESTRUCTOR wil call clear function
+SQueue::~SQueue()
+{
+  clear();
 }
-void SQueue::clear() {
-    
-}
-SQueue::SQueue(const SQueue& rhs) {
-  
+// clear funciton will deaallocate the memory and set the m_heap to nullptr
+void SQueue::clear()
+{
+  while (m_heap != nullptr)
+  {
+    Post *temp = m_heap;
+    m_heap = m_heap->m_right;
+    delete temp;
+  }
+  m_size = 0;
+  m_heap = nullptr;
 }
 
-SQueue& SQueue::operator=(const SQueue& rhs) {
-  
-}
-void SQueue::mergeWithQueue(SQueue& rhs) {
+// copy constructor
+SQueue::SQueue(const SQueue &rhs)
+{
+  m_heap = nullptr; // set the copyTo pointer to null
+  m_size = 0;       // setting the copyTo size to 0
+  m_priorFunc = rhs.m_priorFunc;
+  m_heapType = rhs.m_heapType;
+  m_structure = rhs.m_structure;
 
-}
-
-bool SQueue::insertPost(const Post& post) {
-    
-}
-
-int SQueue::numPosts() const {
-  
-}
-prifn_t SQueue::getPriorityFn() const {
-  
-}
-Post SQueue::getNextPost() {
-    
-}
-void SQueue::setPriorityFn(prifn_t priFn, HEAPTYPE heapType) {
-                      
-}
-void SQueue::setStructure(STRUCTURE structure){
-    
-}
-STRUCTURE SQueue::getStructure() const {
-  
-}
-HEAPTYPE SQueue::getHeapType() const {
-
-}
-void SQueue::printPostsQueue() const {
-  
+  // Deep copy all nodes
+  if (rhs.m_heap != nullptr)
+  {
+    Post *current = rhs.m_heap; // created a pointer of current heap root
+    while (current != nullptr)
+    {
+      insertPost(*current);
+      current = current->m_right;
+    }
+  }
 }
 
-void SQueue::dump() const {
-  if (m_size == 0) {
-    cout << "Empty heap.\n" ;
-  } else {
-      dump(m_heap);
+// deep copy using operator
+SQueue &SQueue::operator=(const SQueue &rhs)
+{
+  if (this != &rhs)
+  {          // Prevent self-assignment
+    clear(); // Delete old content
+
+    m_priorFunc = rhs.m_priorFunc;
+    m_heapType = rhs.m_heapType;
+    m_structure = rhs.m_structure;
+
+    // Deep copy all nodes
+    if (rhs.m_heap != nullptr)
+    {
+      Post *current = rhs.m_heap;
+      while (current != nullptr)
+      {
+        insertPost(*current);
+        current = current->m_right;
+      }
+    }
+  }
+  return *this;
+}
+
+// merging two ques
+void SQueue::mergeWithQueue(SQueue &rhs)
+{
+  // error checking in case of merging with same que
+  if (this == &rhs)
+  {
+    throw domain_error("Cannot merge a queue with itself");
+  }
+  // error checking in case of using non pertinent priority func or structuire
+  if (m_priorFunc != rhs.m_priorFunc || m_structure != rhs.m_structure)
+  {
+    throw domain_error("Cannot merge queues with different priority functions or structures");
+  }
+  // using healper function for merge
+  m_heap = merge(m_heap, rhs.m_heap);
+  m_size += rhs.m_size;
+  rhs.m_heap = nullptr;
+  rhs.m_size = 0;
+}
+// insert a post in the que
+bool SQueue::insertPost(const Post &post)
+{
+  if (m_priorFunc(post) == 0)
+  {
+    return false; // Invalid priority value
+  }
+
+  Post *newPost = new Post(post);
+  m_heap = merge(m_heap, newPost);
+  m_size++;
+  return true;
+}
+
+// return the number of posts in que
+int SQueue::numPosts() const
+{
+  return m_size;
+}
+
+// gettting currnt prioty function
+prifn_t SQueue::getPriorityFn() const
+{
+  return m_priorFunc;
+}
+// extract and return the highest post
+Post SQueue::getNextPost()
+{
+  if (m_size == 0)
+  {
+    throw out_of_range("Queue is empty");
+  }
+
+  Post *highest = m_heap;
+  m_heap = merge(m_heap->m_left, m_heap->m_right);
+  m_size--;
+
+  Post result = *highest;
+  delete highest;
+  return result;
+}
+
+// set new priorty functon and rebuild heap
+void SQueue::setPriorityFn(prifn_t priFn, HEAPTYPE heapType)
+{
+  m_priorFunc = priFn;
+  m_heapType = heapType;
+
+  // Collect all nodes
+  Post *current = m_heap;
+  Post *nodes = nullptr;
+  while (current != nullptr)
+  {
+    Post *temp = current;
+    current = current->m_right;
+    temp->m_left = temp->m_right = nullptr;
+    temp->m_npl = 0;
+    temp->m_right = nodes;
+    nodes = temp;
+  }
+  m_heap = nullptr;
+
+  // Rebuild heap
+  current = nodes;
+  while (current != nullptr)
+  {
+    Post *temp = current;
+    current = current->m_right;
+    temp->m_right = nullptr;
+    m_heap = merge(m_heap, temp);
+  }
+}
+
+// set new structre and rebulid hep
+void SQueue::setStructure(STRUCTURE structure)
+{
+  m_structure = structure;
+  setPriorityFn(m_priorFunc, m_heapType); // Rebuild using current priority function
+}
+
+// getter of stucture
+STRUCTURE SQueue::getStructure() const
+{
+  return m_structure;
+}
+
+// getter of heap type
+HEAPTYPE SQueue::getHeapType() const
+{
+  return m_heapType;
+}
+void SQueue::printPostsQueue() const
+{
+  cout << "Contents of the queue:" << endl;
+  printPreorder(m_heap);
+}
+
+void SQueue::dump() const
+{
+  if (m_size == 0)
+  {
+    cout << "Empty heap.\n";
+  }
+  else
+  {
+    dump(m_heap);
   }
   cout << endl;
 }
-void SQueue::dump(Post *pos) const {
-  if ( pos != nullptr ) {
+void SQueue::dump(Post *pos) const
+{
+  if (pos != nullptr)
+  {
     cout << "(";
     dump(pos->m_left);
     if (m_structure == SKEW)
-        cout << m_priorFunc(*pos) << ":" << pos->m_postID;
+      cout << m_priorFunc(*pos) << ":" << pos->m_postID;
     else
-        cout << m_priorFunc(*pos) << ":" << pos->m_postID << ":" << pos->m_npl;
+      cout << m_priorFunc(*pos) << ":" << pos->m_postID << ":" << pos->m_npl;
     dump(pos->m_right);
     cout << ")";
   }
 }
 
-ostream& operator<<(ostream& sout, const Post& post) {
-  sout  << "Post#: " << post.getPostID()
-        << ", likes#: " << post.getNumLikes() 
-        << ", connect level: " << post.getConnectLevel();
+ostream &operator<<(ostream &sout, const Post &post)
+{
+  sout << "Post#: " << post.getPostID()
+       << ", likes#: " << post.getNumLikes()
+       << ", connect level: " << post.getConnectLevel();
   return sout;
+}
+
+// Private helper function to merge two heaps
+Post *SQueue::merge(Post *h1, Post *h2)
+{
+  if (h1 == nullptr)
+    return h2;
+  if (h2 == nullptr)
+    return h1;
+
+  // Ensure h1 has higher priority
+  if ((m_heapType == MAXHEAP && m_priorFunc(*h1) < m_priorFunc(*h2)) ||
+      (m_heapType == MINHEAP && m_priorFunc(*h1) > m_priorFunc(*h2)))
+  {
+    Post *temp = h1;
+    h1 = h2;
+    h2 = temp;
+  }
+
+  if (m_structure == SKEW)
+  {
+    // Skew heap: swap children
+    Post *temp = h1->m_left;
+    h1->m_left = h1->m_right;
+    h1->m_right = merge(h2, temp);
+  }
+  else
+  { // LEFTIST
+    h1->m_right = merge(h1->m_right, h2);
+    // Update NPL and swap if necessary
+    int leftNPL = (h1->m_left) ? h1->m_left->m_npl : -1;
+    int rightNPL = (h1->m_right) ? h1->m_right->m_npl : -1;
+    h1->m_npl = min(leftNPL, rightNPL) + 1;
+
+    if (leftNPL < rightNPL)
+    {
+      Post *temp = h1->m_left;
+      h1->m_left = h1->m_right;
+      h1->m_right = temp;
+    }
+  }
+  return h1;
+}
+
+// Helper function for preorder printing
+void SQueue::printPreorder(Post *node) const
+{
+  if (node != nullptr)
+  {
+    cout << "[" << m_priorFunc(*node) << "] " << *node << endl;
+    printPreorder(node->m_left);
+    printPreorder(node->m_right);
+  }
 }
